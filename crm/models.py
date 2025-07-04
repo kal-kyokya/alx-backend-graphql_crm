1e5f7ec6c7007a90d40780e710bd865fcdd0d865
kal-kyokya <kalkyokya4@gmail.com>
2025-07-04 13:25:18 +0000

    Create the models associated with the 'crm' app.

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+# ---------------------------------------
+# Collection of registered clients 
+# ---------------------------------------
+class Customer(models.Model):
+    """Represents the DB table containing all customer informations.
+    Inheritance:
+    	models.Model: Base class enabling ORM management.
+    """
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True
+    )
+
+
+# ---------------------------------------
+# Collection of offered products
+# ---------------------------------------
+class Product(models.Model):
+    """Represents the Db table with all product records.
+    Inheritance:
+    	models.Model: Enables mapping of this class on the database.
+    """
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+
+# ---------------------------------------
+# Collection of all placed orders
+# ---------------------------------------
+class Order(models.Model):
+    """Represents the Order table from the backend database.
+    Inheritance:
+    	models.Model: Ensures this class is mapped onto the right DB table.
+    """
+    customer_id = models.ForeignKey(Customer, om_delete=models.CASCADE)
+    product_id = models.ManyToManyField(Product)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.0
+    )
