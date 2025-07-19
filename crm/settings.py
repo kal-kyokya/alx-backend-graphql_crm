a31a68bb1bba2ef9fe8aa1ac5175e3525d466baa
kal-kyokya <kalkyokya4@gmail.com>
2025-07-19 12:22:27 +0000

    Complete the set up of celery with redis for the Django App.

--- crm/settings.py
+++ crm/settings.py
@@ -8,3 +8,8 @@ CRONJOBS = [
 # Enable logging to a different file
 # CRONTAB_COMMAND_PREFIX = 'PATH=/usr/bin:/bin:/usr/sbin:/sbin'
 # Optional for env clarity
+
+INSTALLED_APPS += ['django_celery_beat']
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
