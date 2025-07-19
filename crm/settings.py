26fab191fb659a7f1a8a60cc9f9e212a98fbf1ab
kal-kyokya <kalkyokya4@gmail.com>
2025-07-19 12:54:50 +0000

    Schedule with Celery Beat.

--- crm/settings.py
+++ crm/settings.py
@@ -1,3 +1,6 @@
+from celery.schedules import crontab
+
+
 INSTALLED_APPS += ['django_crontab']
 
 CRONJOBS = [
@@ -13,3 +16,10 @@ INSTALLED_APPS += ['django_celery_beat']
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0)
+    }
+}
